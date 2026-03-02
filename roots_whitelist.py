3afb06f2997114b4b3ac847065d5a17d82afe6f9
FolkaDev <58298060+Folka2134@users.noreply.github.com>
2026-03-02 20:20:58 +0000

    Add roots_whitelist.py file

--- roots_whitelist.py
+++ roots_whitelist.py
@@ -1,13 +1,13 @@
 import pandas as pd
 import sys
 
-# Usage: python3 get_seeds.py program.csv
+# Usage: python3 roots_whitelist.py program.csv
 df = pd.read_csv(sys.argv[1])
 # Extract only where submission is True
 seeds = df[df['eligible_for_submission'] == True]['identifier']
 
 # Clean and Save
-with open('seeds.txt', 'w') as f:
+with open('whitelist_roots.txt', 'w') as f:
     for item in seeds.unique():
         # Strip *. and https:// to get the base root
         clean = item.replace('*.', '').replace('https://', '').split('/')[0]
